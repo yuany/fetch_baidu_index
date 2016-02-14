aa6136286e288e47ca577fca722b05db88afedf1
longzhiwen888 <longzhiwen-478@163.com>
2016-02-14 13:56:18 +0800

    Update config.py
    修复代码

--- baidu_index/config.py
+++ baidu_index/config.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# user_name = 'your_account'
-# password = 'your_password'
+user_name = 'your_account'
+password = 'your_password'
 login_url = ('https://passport.baidu.com/v2/?login&tpl=mn&u='
              'http%3A%2F%2Fwww.baidu.com%2F')
 one_week_trend_url = ('http://index.baidu.com/?tpl=trend&type=0'
