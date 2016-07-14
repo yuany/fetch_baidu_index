8e73e9a77079234da83e2b73e15496a5173338dd
chenhailong <chenhailong@hothuati.com>
2016-07-14 10:25:04 +0800

    增强本项目的功能，更灵活

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 目的：本项目主要是用来抓取百度指数的值。因为百度指数为了防止抓取，将数字转成了图片。
 
+# 运行步骤:
+1.修改baidu_index/config.py要启用的浏览器driver, 因为有些人PhantomJS配置可能有问题，默认使用Firefox(容易配置).
+  具体参考selenium的浏览器环境配置
+2.修改baidu_index/config.py里面的百度账号跟密码
 
+3.baidu_index/config.py 不配置start_date和end_date，可以查询到这个关键词数据的最大区间
 
+4. python main.py
 
--- baidu_index/browser.py
+++ baidu_index/browser.py
@@ -10,11 +10,18 @@ from selenium import webdriver
 
 import config
 from .api import Api
+from .utils.log import logger
+from .multi_thread import WorkManager
 
 
 class BaiduBrowser(object):
     def __init__(self, cookie_json='', check_login=True):
-        self.browser = webdriver.Firefox()
+        if not config.browser_driver:
+            browser_driver_name = 'Firefox'
+        else:
+            browser_driver_name = config.browser_driver
+        browser_driver_class = getattr(webdriver, browser_driver_name)
+        self.browser = browser_driver_class()
         self.browser.set_page_load_timeout(50)
         self.browser.set_script_timeout(10)
         self.user_name = config.user_name
@@ -60,7 +67,22 @@ class BaiduBrowser(object):
         end_date = end_date.strftime("%Y-%m-%d")
         return start_date, end_date, date_list
 
-    def get_baidu_index_by_date_range(self, keyword, start_date, end_date):
+    def get_one_day_index(self, date, url):
+        try_num = 0
+        try_max_num = 5
+        while try_num < try_max_num:
+            try:
+                try_num += 1
+                img_url, val_info = self.api.get_index_show_html(url)
+                value = self.api.get_value_from_url(img_url, val_info)
+                break
+            except:
+                pass
+        logger.info('date:%s, value:%s' % (date, value))
+        return value.replace(',', '')
+
+    def get_baidu_index_by_date_range(self, keyword, start_date, end_date,
+                                      type_name):
         print 'start_date:%s, end_date:%s' % (start_date, end_date)
         url = config.time_range_trend_url.format(
             start_date=start_date, end_date=end_date,
@@ -77,27 +99,25 @@ class BaiduBrowser(object):
         url = config.all_index_url.format(
             res=res, res2=res2, start_date=start_date, end_date=end_date
         )
-        print url
         all_index_info = self.api.get_all_index_html(url)
-        enc_s = all_index_info['data']['all'][0]['userIndexes_enc'].split(',')
+        enc_s = all_index_info['data'][type_name][0]['userIndexes_enc'].split(',')
 
-        baidu_index_dict = dict()
+        wm = WorkManager(config.num_of_threads)
+
         for index, _ in enumerate(enc_s):
             url = config.index_show_url.format(
                 res=res, res2=res2, enc_index=_, t=int(time.time()) * 1000
             )
-            try_num = 0
-            try_max_num = 5
-            while try_num < try_max_num:
-                try:
-                    try_num += 1
-                    img_url, val_info = self.api.get_index_show_html(url)
-                    value = self.api.get_value_from_url(img_url, val_info)
-                    break
-                except:
-                    pass
-            baidu_index_dict[date_list[index]] = value.replace(',', '')
-            print keyword, date_list[index], value.replace(',', '')
+            date = date_list[index]
+            wm.add_job(date, self.get_one_day_index, date, url)
+
+        wm.start()
+        wm.wait_for_complete()
+        baidu_index_dict = wm.get_all_result_dict_from_queue()
+        # date_list = sorted(baidu_index_dict.keys())
+        # for date in date_list:
+        #     logger.info('date:%s, value:%s' % (date, baidu_index_dict[date]))
+
         return baidu_index_dict
 
     def _get_index_period(self, keyword):
@@ -117,32 +137,43 @@ class BaiduBrowser(object):
             res=res, res2=res2, start_date=start_date, end_date=end_date
         )
         all_index_info = self.api.get_all_index_html(url)
-        start_date, end_date = all_index_info['data']['all'][0]['period'].split('|')
+        start_date, end_date = all_index_info['data']['all'][0][
+            'period'].split('|')
         # 重置start_date, end_date，以api返回的为准
         start_date, end_date, date_list = self.get_date_info(
             start_date, end_date
         )
-        print 'all_start_date:%s, all_end_date:%s' % (start_date, end_date)
+        logger.info('all_start_date:%s, all_end_date:%s' % (start_date, end_date))
         return date_list
 
-    def get_baidu_index(self, keyword):
-        date_list = self._get_index_period(keyword)
+    def get_baidu_index(self, keyword, type_name):
+        if config.start_date and config.end_date:
+            _, _, date_list = self.get_date_info(
+                start_date=config.start_date, end_date=config.end_date
+            )
+        else:
+            # 配置文件不配置start_date和end_date，可以查询到这个关键词数据的最大区间
+            date_list = self._get_index_period(keyword)
 
         baidu_index_dict = dict()
         start = 0
-        skip = 90
+        skip = 180
         end = len(date_list)
         while start < end:
             try:
                 start_date = date_list[start]
-                end_date = date_list[start + skip]
+                if start + skip >= end - 1:
+                    end_date = date_list[-1]
+                else:
+                    end_date = date_list[start + skip]
                 result = self.get_baidu_index_by_date_range(
-                    keyword, start_date, end_date
+                    keyword, start_date, end_date, type_name
                 )
                 baidu_index_dict.update(result)
                 start += skip + 1
             except:
                 import traceback
+
                 print traceback.format_exc()
         return baidu_index_dict
 
